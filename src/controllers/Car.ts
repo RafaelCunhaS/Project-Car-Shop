9653dde0762cef43581ed5afed8c278adb3915cb
Rafael Santos <rafaelcs_94@hotmail.com>
2022-07-29 15:58:32 -0300

    feat: :sparkles: adds service and controller classes for Car

---
+++ src/controllers/Car.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import { IService } from '../interfaces/IService';
+import { ICar } from '../interfaces/ICar';
+
+export default class CarController {
+  constructor(private _service: IService<ICar>) { }
+
+  public async create(req: Request & { body: ICar }, res: Response<ICar>) {
+    const results = await this._service.create(req.body);
+    return res.status(201).json(results);
+  }
+
+  public async readOne(req: Request, res: Response<ICar>) {
+    const result = await this._service.readOne(req.params.id);
+    return res.status(200).json(result);
+  }
+
+  public async read(_req: Request, res: Response<ICar[]>) {
+    const result = await this._service.read();
+    return res.status(200).json(result);
+  }
+
+  public async update(req: Request & { body: ICar }, res: Response<ICar>) {
+    const result = await this._service.update(req.params.id, req.body);
+    return res.status(200).json(result);
+  }
+
+  public async delete(req: Request, res: Response<ICar>) {
+    const result = await this._service.delete(req.params.id);
+    return res.status(200).json(result);
+  }
+}
---
+++ src/services/Car.ts
@@ -0,0 +1,47 @@
+import { ICar, CarZodSchema } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/catalog';
+import { IService } from '../interfaces/IService';
+
+class CarService implements IService<ICar> {
+  constructor(private _model:IModel<ICar>) {}
+
+  public async create(obj:ICar): Promise<ICar> {
+    const parsed = CarZodSchema.safeParse(obj);
+
+    if (!parsed.success) throw parsed.error;
+
+    return this._model.create(obj);
+  }
+
+  public async readOne(_id:string):Promise<ICar> {
+    const car = await this._model.readOne(_id);
+
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    
+    return car;
+  }
+
+  public async read(): Promise<ICar[]> {
+    const cars = await this._model.read();
+    return cars;
+  }
+
+  public async update(_id: string, obj: ICar): Promise<ICar> {
+    const carUpdated = await this._model.update(_id, obj);
+
+    if (!carUpdated) throw Error(ErrorTypes.EntityNotFound);
+
+    return carUpdated;
+  }
+
+  public async delete(_id: string): Promise<ICar> {
+    const carDeleted = await this._model.delete(_id);
+
+    if (!carDeleted) throw Error(ErrorTypes.EntityNotFound);
+
+    return carDeleted;
+  }
+}
+
+export default CarService;
